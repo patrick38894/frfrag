2a8134023ec2c80464d27ccef6cd48a33193ded3
H Forrest alexander <h.forrest.alexander@gmail.com>
2014-05-22 16:17:56 -0700

    another tcing, but not necessarily useful nat impl

--- V2/Nat.hs
+++ V2/Nat.hs
@@ -7,6 +7,8 @@
              RankNTypes,
              UndecidableInstances #-}
 
+import GHC.Exts
+
 data Z = Z
 data S t = S t
 type One = S Z
@@ -24,31 +26,29 @@ n2 = S . S $ Z
 n3 = S . S . S $ Z
 n4 = S . S . S . S $ Z
 
+class IsNat n where
+    nat :: n -> Int
+    fromInt :: Int -> n
+
+instance IsNat Z where
+    nat Z = 0
+    fromInt 0 = Z
+
+instance IsNat t => IsNat (S t) where
+    nat (S t) = 1 + nat t
+    fromInt n =  S (fromInt (n - 1))
+
 class IsDim a where
     dims :: a -> [Int]
     fromDims :: [Int] -> a
 
-data Dim :: * -> * where Dim :: IsDim n => n -> Dim n
+data Dim :: * -> * where 
+    MDim :: IsDim n => n -> Dim n
+    Dim :: IsNat n => n -> Dim n
 
-instance IsDim n => IsDim (Dim n) where
-    dims (Dim n) = dims n
-    fromDims n = Dim (fromDims n)
-
-instance IsDim One where
-    dims = const [1]
-    fromDims [1] = S Z
-
-instance IsDim Two where 
-    dims = const [2]
-    fromDims [2] = S (S Z)
-
-instance IsDim Three where
-    dims = const [3]
-    fromDims [3] = S (S (S Z))
-
-instance IsDim Four where 
-    dims = const [4]
-    fromDims [4] = n4
+instance IsNat n => IsDim (Dim n) where
+    dims (Dim n) = [nat n]
+    fromDims [n] = Dim (fromInt n)
 
 instance (IsDim n, IsDim m) => IsDim (n, m) where
     dims (n, m) = dims n ++ dims m
@@ -60,10 +60,16 @@ instance GenType Bool N1 where getRep a = Bool
 instance GenType Int N1 where getRep a = Int
 instance GenType Float N1 where getRep a = Float
 
-instance GenType a N1 => GenType [a] n where
-    getRep xs = let l = fromDims [length xs]
+instance (GenType a N1, IsNat n) => GenType [a] n where
+    getRep xs = let n = fromDims [length xs]
                     t = getRep (head xs)
-                in Vec l t
+                in Vec n t
+
+instance (GenType a N1, IsNat n, IsNat m) => GenType [[a]] (n,m) where
+    getRep xs = let n = fromDims [length xs]
+                    m = fromDims [(the . map length $ xs)]
+                    t = getRep (head (head xs))
+                in Mat n m t
 
 scalarRep :: Rep [a] n -> Rep a N1
 scalarRep = undefined
@@ -72,10 +78,10 @@ data Rep :: * -> * -> * where
     Bool :: Rep Bool N1
     Int :: Rep Int N1
     Float :: Rep Float N1
-    Vec :: Dim n -> Rep a N1 -> Rep [a] n
+    Vec ::  Dim n -> Rep a N1 -> Rep [a] n
     Mat :: Dim n -> Dim m -> Rep a N1 -> Rep [[a]] (n, m)
     Swiz :: GenType b n => Rep [a] n -> String -> Rep b m
     Gen :: GenType a n => Rep a n
 
-x :: (IsDim n, GenType a N1, GenType [a] n) => Rep [a] n -> Rep a N1
+x :: (IsDim n, IsNat n, GenType a N1, GenType [a] n) => Rep [a] n -> Rep a N1
 x v = case v of Vec n xs -> undefined
