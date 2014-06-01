7fb4930c5058afd9ddd826dd6476268998bb2d03
H Forrest alexander <h.forrest.alexander@gmail.com>
2014-05-31 18:55:02 -0700

    not working on this right now anyway

--- Sequence.hs
+++ Sequence.hs
@@ -1,11 +1,8 @@
 module Sequence where
-import Monad
+import Fragment
 import Language
 
 data Loop = Sequence Int Int Int
           | Map Expr Loop
           | Zip Loop Loop
           | Fold Expr Expr Loop
-
-seqLoop :: Loop -> Interpret Stmt
-seqLoop = undefined
