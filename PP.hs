68a4cf032fea3ce2dfce0f2f79068d4267dd19fd
H Forrest alexander <h.forrest.alexander@gmail.com>
2014-06-01 22:53:00 -0700

    consolidate stupid printer stuff

--- PP.hs
+++
@@ -1,8 +0,0 @@
-class PPNative a where pp :: a -> Doc
-instance PPNative Int where pp = int
-instance PPNative Bool where pp x = text $ if x then "true" else "false"
-instance PPNative Float where pp = float
-instance PPNative a => PPNative [a] where
-    pp = sep . punctuate comma . map pp
-
-
--- Printer.hs
+++ Printer.hs
@@ -2,6 +2,14 @@ module Printer where
 import Language
 import Text.PrettyPrint.HughesPJ
 
+class PPNative a where pp :: a -> Doc
+instance PPNative Int where pp = int
+instance PPNative Bool where pp x = text $ if x then "true" else "false"
+instance PPNative Float where pp = float
+instance PPNative a => PPNative [a] where
+    pp = sep . punctuate comma . map pp
+
+
 pptag t = case t of
     Type Int 1 1 -> text "int"
     Type Float 1 1 -> text "float"
