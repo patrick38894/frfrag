16bbd4d9c5b70975dc13296abd8d5e50df468103
H Forrest alexander <h.forrest.alexander@gmail.com>
2014-06-04 13:47:48 -0700

    added clock

--- Render.hs
+++ Render.hs
@@ -1,7 +1,9 @@
 module Render where
 import Pipes
+import Control.Monad
+import qualified Pipes.Prelude as P
 import Language
-
+import System.Clock
 
 type Uniform = (Type, Tagged)
 
@@ -14,6 +16,55 @@ data Beh a = Const a
 data Event a = Event (Maybe a)
 
 
+test = runEffect $ Pipes.for (clock 20) (lift . putStrLn . Prelude.show)
+test2 = runEffect $ waitClock 60 (count 0) >-> P.print
+
+
+count :: Monad m => Int -> Producer Int m r
+count n = yield n >> count (n+1)
+
+-- Given a frequency, produce a stream of doubles counting by the period corresponding to it.
+--clock :: Double -> Producer Double IO Double
+clock hz = do 
+    s <- lift (monoTime)
+    count 1 >-> ticks s hz
+
+--ticks :: Double -> Pipe Double IO Double Double
+ticks s hz = do
+    n <- await
+    t <- lift $ waitUntil $ (+s) $ (*period hz) $ fromIntegral $ n
+    yield t
+    ticks (s+period hz) hz
+
+seconds :: TimeSpec -> Double
+seconds (TimeSpec s ns) = fromIntegral s + fromIntegral ns / 10 ** 9
+
+timeSpec :: Double -> TimeSpec
+timeSpec d = let (s,ns) = properFraction d in
+    TimeSpec s (round $ ns * 10 ** 9)
+
+period :: Double -> Double
+period hz = 1 / hz
+
+monoTime :: IO Double
+monoTime = fmap seconds (getTime Realtime)
+
+waitUntil :: Double -> IO Double
+waitUntil t = do
+    t' <- monoTime
+    unless (t' > t) (void $ waitUntil t)
+    return t'
+
+-- Wait until a clock signal before passing along next value
+waitClock :: Double -> Producer a IO r -> Producer (Double, a) IO r
+waitClock hz = P.zip (clock hz)
+
+
+-- Get user input from GLFW,
+--  including mouse, keyboard, window resize and close events
+getUserInput :: Producer Input IO ()
+getUserInput = undefined
+
 -- If given, update the shader to a new one, compiling and linking the new shader program.
 updateShader :: Consumer String IO ()
 updateShader = undefined
@@ -21,19 +72,6 @@ updateShader = undefined
 -- If given, update all supplied uniform values to new ones.
 updateUniforms :: Consumer [Maybe Uniform] IO ()
 updateUniforms = undefined
-
--- Given a frequency, produce a stream of doubles counting by the period corresponding to it.
-clock :: Double -> Producer Double IO ()
-clock = undefined
-
--- Get user input from GLFW,
---  including mouse, keyboard, window resize and close events
-getUserInput :: Producer Input IO ()
-getUserInput = undefined
-
--- Wait until a clock signal before passing along next value
-waitClock :: Producer Double IO () -> Producer a IO r -> Producer a IO r
-waitClock = undefined
 
 
 -- Render a sequence of fragment shaders, all sharing the same uniform values,
